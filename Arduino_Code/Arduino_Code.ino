09aa95dfb7fb030b6149898a6aa1fbf2751cda46
GeoffSpielman <geoff.spielman21@gmail.com>
2016-06-25 20:13:34 -0400

    Move code to Pi
    Code is not complete, I just need to program from the Pi

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,3 +1,8 @@
+/*
+Created by Geoff Spielman on 2016-06-25.
+Copyright (c) 2016 Geoff Spielman. All rights reserved.
+*/
+
  // put your setup code here, to run once:
   const int btn0 = 2;
   const int btn1 = 7;
@@ -11,6 +16,9 @@
  int btn[] = {btn0, btn1, btn2, btn3};
  int led[] = {led0, led1, led2, led3};
 
+ String recString = "";
+ boolean newAvail = false;
+
 void setup() {
  Serial.begin(9600);
  pinMode(btn0, INPUT);
@@ -21,8 +29,8 @@ void setup() {
  pinMode(led1, OUTPUT);
  pinMode(led2, OUTPUT);
  pinMode(led3, OUTPUT);
- 
- Serial.println("Starting Program");
+ recString.reserve(200);
+ Serial.println("-----Start-----");
  
 }
 
@@ -31,18 +39,25 @@ void loop() {
   for (int i = 0; i < 4; i ++) {
     if (digitalRead(btn[i]) == 1) {
       digitalWrite(led[i], HIGH);
-      Serial.print("LED ");
-      Serial.print(i);
-      Serial.print(" on    ");
+      //Serial.print("LED ");
+      //Serial.print(i);
+      //Serial.print(" on    ");
     }
     else {
       digitalWrite(led[i], LOW);
-      Serial.print("LED ");
-      Serial.print(i);
-      Serial.print(" off    ");
+      //Serial.print("LED ");
+      //Serial.print(i);
+      //Serial.print(" off    ");
     }
       
   }
+  if (newAvail) {
+    //DO SOMETHING
+    recString = "";
+    newAvail = false;
+    
+  }
+  /*
   Serial.println(" ");
   Serial.print("Button0: ");
   Serial.print(digitalRead(btn0));
@@ -53,5 +68,98 @@ void loop() {
   Serial.print("   Button3: ");
   Serial.print(digitalRead(btn3));
   Serial.println(" ");
+  */
 
 }
+
+void serialEvent()
+{
+  while (Serial.available())
+  {  
+    char inChar = (char)Serial.read();
+    recString += inChar;
+    Serial.print("inChar: ");
+    Serial.println(inChar);
+    
+    if (inChar == '|')
+    {
+      Serial.print("Char at 0: ");
+      Serial.println(recString.charAt(0));
+      int endIndex = recString.indexOf("|");
+      if (endIndex == -1)
+      {  
+        endIndex = recString.indexOf("/n");
+      }
+      //PROCESS STRING
+      if (recString.charAt(0) == 'm') {
+        //Message Processing
+        Serial.println("I have recieved a message");
+      }
+      if (recString.charAt(0) == 's') {
+        //Sequence Processing
+        Serial.println("I have received a sequence");
+      }
+
+      
+      newAvail = true;
+    }
+  }
+}
+
+/*
+const int led = 13;
+const int mtrL = 6;
+const int mtrR = 5;
+
+int mL = 0, mR = 0;
+
+String inputString = "";
+boolean needChange = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  inputString.reserve(200);
+  Serial.println("-----Start-----");
+}
+
+void loop() {
+
+  if (needChange)
+  {
+    //Serial.print("Arduino rec: ");
+    //Serial.println(inputString);
+    Serial.print("Left Motor: ");
+    Serial.print(mL);
+    Serial.print("   Right Motor: ");
+    Serial.println(mR);
+
+    analogWrite(mtrL, mL);
+    analogWrite(mtrR, mR);
+    inputString = "";
+    needChange = false;
+  }
+}
+
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n' || inChar == '!') //if 'end with newline' is not enabled on serial monitor, end with !
+    {
+      int commaIndex = inputString.indexOf(",");
+      int endIndex = inputString.indexOf("!");
+      if (endIndex == -1)
+      {  
+        endIndex = inputString.indexOf("/n");
+      }
+      mL = inputString.substring(0, commaIndex).toInt();
+      mR = inputString.substring(commaIndex + 1, endIndex).toInt();
+      needChange = true;
+    } 
+  }
+}
+*/
+
