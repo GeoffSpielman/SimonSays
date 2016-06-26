1599680552477bf67e99dc61fbe2ea5ff3be0bfa
willdavidc <willdavidc@gmail.com>
2016-06-26 05:50:13 -0400

    Removed socketio from js
    Removed socket from js

--- onPiServer.js
+++ onPiServer.js
@@ -1,32 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var val = "-1"
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
-});
-
-io.on('connection', function(socket){
-    
-  console.log('a user connected');
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-    
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-    
-    
-  socket.on('incrementor', function(msg){
-      if (val != msg) {
-          console.log('Incrementor: ' + msg);
-          val = msg;
-      }
-  });
-    
 });
 
 http.listen(3000, function(){
