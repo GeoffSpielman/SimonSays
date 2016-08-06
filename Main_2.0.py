721039683a1685047a0488a986fe216a592e44a8
GeoffSpielman <geoff.spielman21@gmail.com>
2016-08-06 17:32:10 -0400

    remove GPIO

--- Main_2.0.py
+++ Main_2.0.py
@@ -4,51 +4,29 @@
 #Threading from http://www.tutorialspoint.com/python3/python_multithreading.htm
 
 #When program starts
-    #pin modes
     #spaw threads: listen to server 1)sequences 2)
 
-#if something is available on serial
+#if something is available on serial (from arduino)
 #can we send a new sequence to the arduino
 
-
-
-#read morse pin
-    #if change, determine char, word, or sequnce done
-    #if its been off for a while, send off the sequence
-
-#write morse pin
-    #spew what ever is on the queue
-
-
-
-
-
-
+#read from serial for message from arduino
+#write new message to arduino
 
 
 import threading
 import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 
 from socketIO_client import SocketIO, LoggingNamespace
 socketIO = SocketIO('willclark.io:1642')
 
-ledOn = False
 
 exitFlag = 0
 
-
 def on_a_response(*args):
     print(args)
-    if (ledOn):
-        GPIO.output(4, HIGH)
-        ledOn = False
-    else:
-        GPIO.output(4, LOW)
-        ledOn = True
 
 class countThread(threading.Thread):
     def __init__(self, name, interval):
@@ -76,6 +54,8 @@ class socketThread(threading.Thread):
         socketIO.off("sequencePi")
 
 
+
+
 # Create new threads
 thread1 = countThread("simpleCounter", 0.2)
 thread2 = socketThread("socketIOThread", "sequencePi")
@@ -83,6 +63,28 @@ thread2 = socketThread("socketIOThread", "sequencePi")
 # Start new Threads
 thread1.start()
 thread2.start()
+
+#----------Begining of Main Program-------
+
+while (True):
+
+    #If there is anything to read from the Arduino
+    if (ser.inWaiting() != 0):
+
+        recSequence = ser.readline().strip()
+        seqString = recSequence.decode('utf-8')
+        # seqString looks like "3210"
+        # print(seqString)
+        seqLength = len(seqString)
+        oString = "["
+        for i in range(seqLength - 1):
+            oString += (seqString[i] + ",")
+        oString += (seqString[seqLength - 1])
+        oString += "]"
+        # oString looks like "[3,2,1,0]"
+        print(oString)
+        socketIO.emit('sequencePi', oString)
+
 thread1.join()
 thread2.join()
 print ("Exiting Main Thread")
