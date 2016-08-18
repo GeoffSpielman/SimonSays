0ed3eb053c9d4f7a2bb3e010263cad6af90cc8b3
GeoffSpielman <geoff.spielman21@gmail.com>
2016-08-18 03:33:36 +0000

    Code so Will can see 5

--- Main_2.0.py
+++ Main_2.0.py
@@ -16,9 +16,11 @@
 import threading
 import time
 import serial
+import ast
 
-#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 curSequence = int(0)
+prevMessageStamp = int(0101010101010)
 rawSequence = "(u'[8, 8, 8, 8]',)"
 prevRawSequence = "(u'[8, 8, 8, 8]',)"
 
@@ -30,12 +32,26 @@ exitFlag = 0
 
 #every message starts 13 digits and sequence format is {3,2,3,1}
 def on_a_message(*args):
-    print(args[0])
-    print(args[0][:13])
+    global prevMessageStamp
+    if prevMessageStamp != args[0][:13]:
+        prevMessageStamp = args[0][:13]
+        BA = bytearray()
+        BA.extend(map(ord, args[0][13:] + '~'))
+        print(BA)
+        ser.write(BA)
+    
 
 def on_a_sequence(*args):
-    print(args[0])
-    print(args[0][:13])
+    whitelist = set("0123")
+    seqString = ""
+    seqString.join(filter(whitelist.__contains__, str(args[0])))
+    print(seqString)
+    
+    #convert string to args[0]
+    #seqArray = ast.literal_eval(str(args[0]))
+     
+    
+    #print(args[0][:13])
 
     
     # global curSequence
@@ -51,8 +67,25 @@ def on_a_sequence(*args):
     #     # print(prevRawSequence)
     #     # print(rawSequence)
 
+##    rawSequence = str(rawSequence)
+##    rawSequence = rawSequence.split('[')[1]
+##    rawSequence = rawSequence.split(']')[0]    
+##    
+##    
+##    rawArray = rawSequence.split(',')
+##    for i in range (len(rawArray)):
+##        recSequence += rawArray[i]
+##    
+##    #Now looks like "3 2 3 1 2 0"
+##    oString = "s " + recSequence + "|"
+##    seqLength = int((len(recSequence)+1) / 2)
+##    BA = bytearray()
+##    BA.extend(map(ord, oString))
+##    print(BA)
+##    ser.write(BA)
 
-#
+
+
 # class countThread(threading.Thread):
 #     def __init__(self, name, interval):
 #         threading.Thread.__init__(self)
@@ -65,8 +98,6 @@ def on_a_sequence(*args):
 #             print(self.count)
 #             time.sleep(self.interval)
 
-
-
 class SocketThread(threading.Thread):
     def __init__(self, name):
         threading.Thread.__init__(self)
@@ -77,8 +108,6 @@ class SocketThread(threading.Thread):
             socketIO.wait()
             #socketIO.off("messagePi")
             #socketIO.off("sequencePi")
-
-
 
 # Create new threads
 #thread1 = countThread("simpleCounter", 0.2)
@@ -113,53 +142,5 @@ while (True):
     #     socketIO.emit('sequencePi', oString)
 
 socketThread.join()
-print ("Exiting Main Thread")
-##
-##Threads have threaded
-##Exception in thread sequenceThread:
-##Traceback (most recent call last):
-##  File "/usr/lib/python2.7/threading.py", line 810, in __bootstrap_inner
-##    self.run()
-##  File "Main_2.0.py", line 73, in run
-##    socketIO.wait(seconds = 10)
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/socketIO_client/__init__.py", line 246, in wait
-##    self._process_packets()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/socketIO_client/__init__.py", line 268, in _process_packets
-##    for engineIO_packet in self._transport.recv_packet():
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/socketIO_client/transports.py", line 145, in recv_packet
-##    packet_text = self._connection.recv()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 298, in recv
-##    opcode, data = self.recv_data()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 315, in recv_data
-##    opcode, frame = self.recv_data_frame(control_frame)
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 338, in recv_data_frame
-##    return self.cont_frame.extract(frame)
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_abnf.py", line 392, in extract
-##    raise WebSocketPayloadException("cannot decode: " + repr(frame.data))
-##WebSocketPayloadException: cannot decode: '\x81\x18'
-##Exception in thread messageThread:
-##Traceback (most recent call last):
-##  File "/usr/lib/python2.7/threading.py", line 810, in __bootstrap_inner
-##    self.run()
-##  File "Main_2.0.py", line 83, in run
-##    socketIO.wait(seconds = 10)
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/socketIO_client/__init__.py", line 246, in wait
-##    self._process_packets()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/socketIO_client/__init__.py", line 268, in _process_packets
-##    for engineIO_packet in self._transport.recv_packet():
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/socketIO_client/transports.py", line 145, in recv_packet
-##    packet_text = self._connection.recv()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 298, in recv
-##    opcode, data = self.recv_data()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 315, in recv_data
-##    opcode, frame = self.recv_data_frame(control_frame)
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 328, in recv_data_frame
-##    frame = self.recv_frame()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_core.py", line 360, in recv_frame
-##    return self.frame_buffer.recv_frame()
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_abnf.py", line 334, in recv_frame
-##    frame.validate(self.skip_utf8_validation)
-##  File "/home/pi/mySimonSays/venv/local/lib/python2.7/site-packages/websocket/_abnf.py", line 130, in validate
-##    raise WebSocketProtocolException("rsv is not implemented, yet")
-##WebSocketProtocolException: rsv is not implemented, yet
 
+print ("Exiting Main Thread")
